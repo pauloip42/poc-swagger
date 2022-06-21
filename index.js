81357731cf765eb601ecb00350c8a46f0808d363
Paulo Lopes <paulo.junior@nouvenn.com>
2022-06-21 17:09:30 -0300

    :wrench: apis and swagger

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const customers = require('./routes/customersRoute');
 
+const swaggerUI = require('swagger-ui-express');
+
+const YAML = require('yamljs');
+const swaggerDoc = YAML.load('./api-docs/v1.0.yml');
+
 const app = express();
 
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {console.log('Working')});
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.use('/customers', customers);
+app.listen(port, () => {console.log('Working')});
--- routes/customersRoute.js
+++ routes/customersRoute.js
@@ -1,12 +1,7 @@
 const express = require('express');
-const swaggerUI = require('swagger-ui-express');
-const swaggerFile = require('../swagger.json');
-
 const router = express.Router();
 
-router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
-
-router.get('/customers', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Oks')
 });
 
